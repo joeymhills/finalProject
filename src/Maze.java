8706c1e302429d0a8533ea7954b17f11ae293bf4
Mooshiiee <124075722+Mooshiiee@users.noreply.github.com>
2023-05-05 16:23:48 -0400

    Added to maze class

---
+++ src/Maze.java
@@ -0,0 +1,30 @@
+
+public class Maze extends Application {
+	
+	public void start(Stage stage) {
+		
+		/**
+		 * will be the main grid that will contain 
+		 * the GUI for player1 and player 2
+		 */
+		GridPane mainGrid = new GridPane();
+		
+		
+		
+		
+	}
+	
+	
+	/**
+	 * Creates the player GUI that will initiated for player1 and player2 and put side
+	 * by side in the mainGUI. 
+	 * @return
+	 */
+	public GridPane playerGUI() {
+		
+		
+		
+		
+	}
+	
+}
