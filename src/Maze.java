720e392cef0764cf5da7509399d3004cf8614a47
Joey Hills <joeymhills@gmail.com>
2023-05-09 02:22:38 -0400

    Implemented JavaFX and started GUI

--- src/Maze.java
+++ src/Maze.java
@@ -1,5 +1,13 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Maze extends Application {
+	
 	
 	public void start(Stage stage) {
 		
@@ -9,12 +17,25 @@ public class Maze extends Application {
 		 */
 		GridPane mainGrid = new GridPane();
 		
+		Button up = new Button("up");
 		
+		mainGrid.add(up, 0, 1);
 		
+		Scene scene = new Scene(mainGrid, 300, 300);
+	    stage.setTitle("Maze");
+	    stage.setScene(scene);
+	    stage.show();
 		
+		}
+	   
+	public static void main(String[] args) {
+	  
+		launch(args);
+	   
 	}
+		
+}
 	
 	
 
 	
-}
